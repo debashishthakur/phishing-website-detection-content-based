f9d92192cc62f6ec81c6fbcea90abc306725bc6b
Nawaz Hussain <106880163+nawaz0001@users.noreply.github.com>
2022-11-21 18:14:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 # col1, col2 = st.columns([1, 3])
 
-st.title('Phishing Website Detection using Machine Learning Project')
+st.title('Phishing Website Detection using Machine Learning Algorithms')
 
-choice = st.selectbox("Please select your machine learning model",
+choice = st.selectbox("Please select a machine learning model",
                  [
                      'Gaussian Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Random Forest',
                      'AdaBoost', 'Neural Network', 'K-Neighbours'
@@ -43,14 +43,14 @@ else:
 
 url = st.text_input('Enter the URL')
 # check the url is valid or not
-if st.button('Check!'):
+if st.button('Check'):
     try:
         response = re.get(url, verify=False, timeout=4)
         if response.status_code != 200:
             print(". HTTP connection was not successful for the URL: ", url)
         else:
             soup = BeautifulSoup(response.content, "html.parser")
-            vector = [fe.create_vector(soup)]  # it should be 2d array, so I added []
+            vector = [fe.create_vector(soup)]
             result = model.predict(vector)
             if result[0] == 0:
                 st.success("This web page seems a legitimate!")
