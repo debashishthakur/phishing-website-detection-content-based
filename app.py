38ede3d5f7366d5ccf02952672de2535a5ca439e
Nawaz Hussain <106880163+nawaz0001@users.noreply.github.com>
2022-11-22 17:43:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.title('Phishing Website Detection using Machine Learning Algorithms')
 
 choice = st.selectbox("Please select a machine learning model",
                  [
-                     'Gaussian Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Random Forest',
-                     'AdaBoost', 'Neural Network', 'K-Neighbours'
+                     'Gaussian Naive Bayes', 'Decision Tree', 'Random Forest',
+                     'K-Neighbours'
                  ]
                 )
 
@@ -19,23 +19,16 @@ model = ml.nb_model
 
 if choice == 'Gaussian Naive Bayes':
     model = ml.nb_model
-elif choice == 'Support Vector Machine':
-    model = ml.svm_model
 elif choice == 'Decision Tree':
     model = ml.dt_model
 elif choice == 'Random Forest':
     model = ml.rf_model
-elif choice == 'AdaBoost':
-    model = ml.ab_model
-elif choice == 'Neural Network':
-    model = ml.nn_model
 else:
     model = ml.kn_model
     st.write('KN model is selected!')
 
 
 url = st.text_input('Enter the URL')
-# check the url is valid or not
 if st.button('Check'):
     try:
         response = re.get(url, verify=False, timeout=4)
